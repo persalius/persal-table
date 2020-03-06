58ad3cb9e7f903b56612d7fa95f1572b2649c9db
Sergey <persalius@gmail.com>
2020-03-06 17:01:35 +0200

    edit

--- dist/styled/button.js
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-export const Button = styled.button.withConfig({
-  displayName: "button__Button",
-  componentId: "sc-18jb4rm-0"
-})(["width:250px;display:inline-block;background:#1190cb;border:none;outline:none;color:#fff;font:14px \"Roboto\",sans-serif;padding:10px;border-radius:4px;cursor:pointer;transition:0.3s all;margin-bottom:10px;&:hover{box-shadow:0 3px 4px 1px rgba(17,144,203,0.3);}"]);
